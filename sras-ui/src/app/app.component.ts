fe8f6ca76b7c53a5b64d27f35675c7b5fe72836f
Sharma <2494420@cognizant.com>
2026-05-20 16:53:33 +0530

    Changed constraints of password

--- sras-ui/src/app/app.component.ts
+++ sras-ui/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -16,7 +17,8 @@ import { AuthService } from './services/auth.service';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
-    MatMenuModule
+    MatMenuModule,
+    MatDividerModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
