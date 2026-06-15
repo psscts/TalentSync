7a69a467d6bff6b57fcd3baf5983c996c665a27e
Shetty <2494399@cognizant.com>
2026-06-15 11:07:03 +0530

    update backend api url 1

--- sras-ui/src/environments/environment.prod.ts
+++ sras-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://talentsync-production-6f49.up.railway.app:8080'
+  apiUrl: 'https://talentsync-production-6f49.up.railway.app'
 };
